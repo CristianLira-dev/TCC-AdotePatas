51ee600e60701650b0ab569a1246df8e1d219e9b
CristianLira-dev <cristianfilho2008@outlook.com>
2025-09-25 17:48:56 -0300

    Add files via upload

---
+++ AdotePatas/assets/js/pages/index/patinhas.js
@@ -0,0 +1,21 @@
+        document.addEventListener('DOMContentLoaded',()=>{
+            const adoptBtn = document.getElementById('adoptBtn');
+            const pawPrints = document.getElementById('pawPrints');
+            const counterNumber = document.getElementById('counterNumber');
+            let count = 0;
+            
+            // Criar patinhas aleatórias ao redor do botão
+            (() => {
+                pawPrints.innerHTML = '';
+                
+                for (let i = 0; i < 50; i++) {
+                    const paw = document.createElement('div');
+                    paw.classList.add('paw');
+                    paw.innerHTML = '🐾';
+                    paw.style.left = `${Math.random() * 35}%`;
+                    paw.style.top = `${Math.random() * 35}%`;
+                    paw.style.animationDelay = `${Math.random() * 5}s`;
+                    pawPrints.appendChild(paw);
+                }
+            })();
+        });
