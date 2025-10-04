dbd7b73917db448b1a8880fea99c60784e83410e
Diogo013 <moisesfdelima760@gmail.com>
2025-10-03 21:48:58 -0300

    Telas de Cadastro, Login e inicial Linkadas

---
+++ SQL/adote_patas.sql
@@ -0,0 +1,396 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 30/09/2025 às 17:30
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `adote_patas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `administrador`
+--
+
+CREATE TABLE `administrador` (
+  `id_admin` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `avaliacao`
+--
+
+CREATE TABLE `avaliacao` (
+  `id_avaliacao` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_pet` int(11) NOT NULL,
+  `nota` tinyint(4) DEFAULT NULL CHECK (`nota` between 1 and 5),
+  `data` datetime DEFAULT current_timestamp(),
+  `comentario` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `conversa`
+--
+
+CREATE TABLE `conversa` (
+  `id_conversa` int(11) NOT NULL,
+  `id_usuario1` int(11) NOT NULL,
+  `id_usuario2` int(11) NOT NULL,
+  `id_solicitacao` int(11) NOT NULL,
+  `data_criacao` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `favorito`
+--
+
+CREATE TABLE `favorito` (
+  `id_favorito` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_pet` int(11) NOT NULL,
+  `data` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `mensagem`
+--
+
+CREATE TABLE `mensagem` (
+  `id_mensagem` int(11) NOT NULL,
+  `id_conversa` int(11) NOT NULL,
+  `id_remetente` int(11) NOT NULL,
+  `conteudo` text NOT NULL,
+  `data` datetime DEFAULT current_timestamp(),
+  `denuncia` tinyint(1) DEFAULT 0,
+  `status` enum('ativo','removido') DEFAULT 'ativo'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `ong`
+--
+
+CREATE TABLE `ong` (
+  `id_ong` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL COMMENT 'Nome completo da ONG',
+  `cnpj` varchar(20) NOT NULL COMMENT 'CNPJ da ONG',
+  `email` varchar(255) NOT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `endereco` text DEFAULT NULL,
+  `data_cadastro` timestamp NOT NULL DEFAULT current_timestamp(),
+  `senha` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `ong`
+--
+
+INSERT INTO `ong` (`id_ong`, `nome`, `cnpj`, `email`, `telefone`, `endereco`, `data_cadastro`, `senha`) VALUES
+(1, 'RobertoCarlos cachorros', '22093212000101', 'Robertos@gmail.com', NULL, NULL, '2025-09-30 05:13:34', '$2y$10$FwlhINPn9SmNpHq6P1Bz5uJMnOsS3X0me1lzMGZRCLV6fCDa23oQW');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pet`
+--
+
+CREATE TABLE `pet` (
+  `id_pet` int(11) NOT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `idade` int(11) DEFAULT NULL,
+  `cor` varchar(50) DEFAULT NULL,
+  `raca` varchar(50) DEFAULT NULL,
+  `porte` enum('pequeno','medio','grande') DEFAULT NULL,
+  `comportamento` text DEFAULT NULL,
+  `status_vacinacao` enum('sim','nao') DEFAULT NULL,
+  `especie` enum('cachorro','gato','outro') DEFAULT NULL,
+  `status_castracao` enum('sim','nao') DEFAULT NULL,
+  `foto` varchar(255) DEFAULT NULL,
+  `status_disponibilidade` enum('disponivel','adotado','indisponivel') DEFAULT 'disponivel',
+  `id_usuario_fk` int(11) DEFAULT NULL COMMENT 'Chave estrangeira para o Usuário (Doador PF)',
+  `id_ong_fk` int(11) DEFAULT NULL COMMENT 'Chave estrangeira para a ONG Doadora'
+) ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `relatorio`
+--
+
+CREATE TABLE `relatorio` (
+  `id_relatorio` int(11) NOT NULL,
+  `id_admin` int(11) NOT NULL,
+  `tipo` varchar(50) DEFAULT NULL,
+  `data` datetime DEFAULT current_timestamp(),
+  `conteudo` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `solicitacao`
+--
+
+CREATE TABLE `solicitacao` (
+  `id_solicitacao` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_pet` int(11) NOT NULL,
+  `status_solicitacao` enum('pendente','aprovada','rejeitada') DEFAULT 'pendente',
+  `data` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `estado` varchar(50) DEFAULT NULL,
+  `cidade` varchar(50) DEFAULT NULL,
+  `tipo` enum('adotante','doador') NOT NULL,
+  `cpf` int(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `email`, `senha`, `nome`, `estado`, `cidade`, `tipo`, `cpf`) VALUES
+(1, 'fer@gmail.com', '$2y$10$2htIzwn56l8KTiqOYMrwpOgtk0uCTXlowgULtfxKGBvEoTDoYpr1m', 'Diogo Rodrigues', NULL, NULL, 'adotante', 336511);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `administrador`
+--
+ALTER TABLE `administrador`
+  ADD PRIMARY KEY (`id_admin`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Índices de tabela `avaliacao`
+--
+ALTER TABLE `avaliacao`
+  ADD PRIMARY KEY (`id_avaliacao`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_pet` (`id_pet`);
+
+--
+-- Índices de tabela `conversa`
+--
+ALTER TABLE `conversa`
+  ADD PRIMARY KEY (`id_conversa`),
+  ADD KEY `id_usuario1` (`id_usuario1`),
+  ADD KEY `id_usuario2` (`id_usuario2`),
+  ADD KEY `id_solicitacao` (`id_solicitacao`);
+
+--
+-- Índices de tabela `favorito`
+--
+ALTER TABLE `favorito`
+  ADD PRIMARY KEY (`id_favorito`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_pet` (`id_pet`);
+
+--
+-- Índices de tabela `mensagem`
+--
+ALTER TABLE `mensagem`
+  ADD PRIMARY KEY (`id_mensagem`),
+  ADD KEY `id_conversa` (`id_conversa`),
+  ADD KEY `id_remetente` (`id_remetente`);
+
+--
+-- Índices de tabela `ong`
+--
+ALTER TABLE `ong`
+  ADD PRIMARY KEY (`id_ong`),
+  ADD UNIQUE KEY `cnpj` (`cnpj`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Índices de tabela `pet`
+--
+ALTER TABLE `pet`
+  ADD PRIMARY KEY (`id_pet`),
+  ADD KEY `id_usuario_fk` (`id_usuario_fk`),
+  ADD KEY `id_ong_fk` (`id_ong_fk`);
+
+--
+-- Índices de tabela `relatorio`
+--
+ALTER TABLE `relatorio`
+  ADD PRIMARY KEY (`id_relatorio`),
+  ADD KEY `id_admin` (`id_admin`);
+
+--
+-- Índices de tabela `solicitacao`
+--
+ALTER TABLE `solicitacao`
+  ADD PRIMARY KEY (`id_solicitacao`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_pet` (`id_pet`);
+
+--
+-- Índices de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `administrador`
+--
+ALTER TABLE `administrador`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `avaliacao`
+--
+ALTER TABLE `avaliacao`
+  MODIFY `id_avaliacao` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `conversa`
+--
+ALTER TABLE `conversa`
+  MODIFY `id_conversa` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `favorito`
+--
+ALTER TABLE `favorito`
+  MODIFY `id_favorito` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `mensagem`
+--
+ALTER TABLE `mensagem`
+  MODIFY `id_mensagem` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `ong`
+--
+ALTER TABLE `ong`
+  MODIFY `id_ong` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `pet`
+--
+ALTER TABLE `pet`
+  MODIFY `id_pet` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `relatorio`
+--
+ALTER TABLE `relatorio`
+  MODIFY `id_relatorio` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `solicitacao`
+--
+ALTER TABLE `solicitacao`
+  MODIFY `id_solicitacao` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `avaliacao`
+--
+ALTER TABLE `avaliacao`
+  ADD CONSTRAINT `avaliacao_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE,
+  ADD CONSTRAINT `avaliacao_ibfk_2` FOREIGN KEY (`id_pet`) REFERENCES `pet` (`id_pet`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `conversa`
+--
+ALTER TABLE `conversa`
+  ADD CONSTRAINT `conversa_ibfk_1` FOREIGN KEY (`id_usuario1`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE,
+  ADD CONSTRAINT `conversa_ibfk_2` FOREIGN KEY (`id_usuario2`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE,
+  ADD CONSTRAINT `conversa_ibfk_3` FOREIGN KEY (`id_solicitacao`) REFERENCES `solicitacao` (`id_solicitacao`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `favorito`
+--
+ALTER TABLE `favorito`
+  ADD CONSTRAINT `favorito_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE,
+  ADD CONSTRAINT `favorito_ibfk_2` FOREIGN KEY (`id_pet`) REFERENCES `pet` (`id_pet`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `mensagem`
+--
+ALTER TABLE `mensagem`
+  ADD CONSTRAINT `mensagem_ibfk_1` FOREIGN KEY (`id_conversa`) REFERENCES `conversa` (`id_conversa`) ON DELETE CASCADE,
+  ADD CONSTRAINT `mensagem_ibfk_2` FOREIGN KEY (`id_remetente`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `pet`
+--
+ALTER TABLE `pet`
+  ADD CONSTRAINT `pet_ibfk_1` FOREIGN KEY (`id_usuario_fk`) REFERENCES `usuario` (`id_usuario`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `pet_ibfk_2` FOREIGN KEY (`id_ong_fk`) REFERENCES `ong` (`id_ong`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Restrições para tabelas `relatorio`
+--
+ALTER TABLE `relatorio`
+  ADD CONSTRAINT `relatorio_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `administrador` (`id_admin`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `solicitacao`
+--
+ALTER TABLE `solicitacao`
+  ADD CONSTRAINT `solicitacao_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE,
+  ADD CONSTRAINT `solicitacao_ibfk_2` FOREIGN KEY (`id_pet`) REFERENCES `pet` (`id_pet`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
