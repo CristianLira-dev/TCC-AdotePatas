f766684abe83a31170c9f8e554383526d7f89e46
Cristian Albertino de Lira Filho <cristianfilho2008@outlook.com>
2025-10-04 00:38:59 -0300

    integração da blibioteca do Lottie Files com animação de entrada no site e cachorro andando entre a seção

---
+++ AdotePatas/assets/js/pages/index/loading.js
@@ -0,0 +1,13 @@
+// assets/js/pages/index/loading.js
+
+window.onload = function () {
+  const loadingScreen = document.getElementById("loading");
+
+  if (loadingScreen) {
+    // Define um tempo mínimo de exibição de 2 segundos APÓS a página carregar
+    setTimeout(() => {
+      // Adiciona a classe que inicia a transição de fade-out
+      loadingScreen.classList.add("hidden");
+    }, 3000); // 2000 milissegundos = 2 segundos. Altere este valor conforme necessário.
+  }
+};
